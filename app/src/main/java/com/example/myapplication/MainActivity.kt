0a3b0ee471a83f0888a4825f627e1d0ec17be356
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-08-28 14:55:10 -0600

    Main Activity complete and so is new Production.  Working on passing values from new to Main, to allow for easy GUI tracking

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var new_button : Button // lateinit (essentially null safety feature)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        new_button = findViewById(R.id.new_production) // pointer for button in App
+        new_button.setOnClickListener { do_new_button() }
+
+        // Retrieve the values from the intent from new production
+        val jobNumber = intent.getIntExtra("JOB_NUMBER", -1)
+        val segmentId = intent.getIntExtra("SEGMENT_ID", -1)
+        val quantity = intent.getIntExtra("QUANTITY", -1)
+        // Check if the values are different from their default values (-1) before logging
+        if (jobNumber != -1 && segmentId != -1 && quantity != -1) {
+            Log.i("MainCS3680", "Values from new production - Job Number: $jobNumber, Segment ID: $segmentId, Quantity: $quantity")
+        } else {
+            Log.i("MainCS3680", "No values received from new production")
+        }
+    }
+
+    fun do_new_button(){
+        Log.i("CS3680", "Creating New Production")
+        val intent = Intent(this, NewProduction:: class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/NewProduction.kt
@@ -0,0 +1,65 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.PathIterator.Segment
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NewProduction : AppCompatActivity() {
+    lateinit var save_button : Button // lateinit (essentially null safety feature)
+    lateinit var job_number_edit : EditText
+    lateinit var segment_id : EditText
+    lateinit var quantity_value  : EditText
+    @SuppressLint("MissingInflatedId") // Stops IDE from throwing error when fetching by ID
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_new_production)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Listen for user click on save button
+        save_button = findViewById(R.id.new_save_button) // pointer for button in App
+        job_number_edit = findViewById(R.id.new_job_number)
+        segment_id = findViewById(R.id.new_id_number)
+        quantity_value = findViewById(R.id.new_quantity_number)
+
+        save_button.setOnClickListener {
+            // Converts values to INT (even though they already should be).
+            try {
+                val jobNumber = job_number_edit.text.toString().toInt()
+                val segment = segment_id.text.toString().toInt()
+                val quantity = quantity_value.text.toString().toInt()
+                do_save_button(jobNumber, segment, quantity) // Pass the integer value to the function
+            }
+            catch (e: Exception) {
+                Log.i("Error Thrown", " $e")
+            }
+        }
+
+
+    }
+
+    fun do_save_button(jobNumber: Int, segment: Int, quantity: Int) {
+        // Log the job number to the console
+        Log.i("CS3680", "Saving production with Job Number: $jobNumber, ID: $segment, Quantity $quantity")
+
+        // Saves data from new production Page, and reroutes to Home Screen (MainActivity)
+        val intent = Intent(this, MainActivity::class.java)
+//        intent.putExtras("Job_Number", jobNumber, "Segment_ID", segment, "Quantity", quantity)
+        intent.putExtra("Job_Number", jobNumber)
+        intent.putExtra("Segment_ID", segment)
+        intent.putExtra("Quantity", quantity)
+        startActivity(intent)
+    }
+}
